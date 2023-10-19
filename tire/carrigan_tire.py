f5b81c318c7558b993884783037814df755f9592
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-10-19 02:53:03 +0100

    Update carrigan_tire

--- tire/carrigan_tire.py
+++ tire/carrigan_tire.py
@@ -7,10 +7,3 @@ class Carrigan(Tire):
     
     def needs_service(self):
         return any(wear >= 0.9 for wear in self.wear_sensor_array)
-        #if should_service:
-            #return True
-        #return False
-        '''for value in self.wear_sensor_array:
-            if float(value) >= 0.9:
-                return True
-            return False'''
